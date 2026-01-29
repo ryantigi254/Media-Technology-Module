2953774b670ac141d559946bf1f368ae815ccbb8
ryantigi254 <ryantigi2020@gmail.com>
2026-01-29 11:30:00 +0000

    Add miscellaneous utility modules

---
+++ src/ssc/misc/probabilistic_presence.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from ssc.features.probabilistic_presence import (
+    ProbabilisticPresenceTracker,
+    ProbPresenceConfig,
+    ProbPresenceState,
+)
+
+__all__ = [
+    "ProbabilisticPresenceTracker",
+    "ProbPresenceConfig",
+    "ProbPresenceState",
+]
